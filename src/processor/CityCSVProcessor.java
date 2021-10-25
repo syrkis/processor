81911bccb2192e5cb24dcc8e8618106e28a1e411
Noah Syrkis <noah@syrkis.com>
2021-10-25 19:57:57 +0100

    morph: parser to processor

---
+++ src/processor/CityCSVProcessor.java
@@ -0,0 +1,65 @@
+package processor;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class CityCSVProcessor {
+	
+	public void readAndProcess(File file) {
+		//Try with resource statement (as of Java 8)
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			//Discard header row
+			br.readLine();
+			
+			String line;
+			
+			while ((line = br.readLine()) != null) {
+				// Parse each line
+				String[] rawValues = line.split(",");
+				
+				int id = convertToInt(rawValues[0]);
+				int year = convertToInt(rawValues[1]);
+				String city = convertToString(rawValues[2]);
+				int population = convertToInt(rawValues[3]);
+				
+				// System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
+				
+				//TODO: Extend the program to process entries!
+				processor.CityRecord cityRecord = new processor.CityRecord(id, year, city, population);
+				System.out.println(cityRecord);
+			}
+		} catch (Exception e) {
+			System.err.println("An error occurred:");
+			e.printStackTrace();
+		}
+	}
+	
+	private String cleanRawValue(String rawValue) {
+		return rawValue.trim();
+	}
+	
+	private int convertToInt(String rawValue) {
+		rawValue = cleanRawValue(rawValue);
+		return Integer.parseInt(rawValue);
+	}
+	
+	private String convertToString(String rawValue) {
+		rawValue = cleanRawValue(rawValue);
+		
+		if (rawValue.startsWith("\"") && rawValue.endsWith("\"")) {
+			return rawValue.substring(1, rawValue.length() - 1);
+		}
+		
+		return rawValue;
+	}
+	
+	public static final void main(String[] args) {
+		CityCSVProcessor reader = new CityCSVProcessor();
+		
+		File dataDirectory = new File("data/");
+		File csvFile = new File(dataDirectory, "Cities.csv");
+		
+		reader.readAndProcess(csvFile);
+	}
+}
