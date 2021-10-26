f0bdb8f14c3d5c585cd7eeba4c31ac8afb9cad77
Noah Syrkis <noah@syrkis.com>
2021-10-26 14:46:19 +0100

    Third commit

--- src/processor/CityCSVProcessor.java
+++ src/processor/CityCSVProcessor.java
@@ -3,9 +3,14 @@ package processor;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class CityCSVProcessor {
-	
+
 	public void readAndProcess(File file) {
 		//Try with resource statement (as of Java 8)
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
@@ -13,7 +18,9 @@ public class CityCSVProcessor {
 			br.readLine();
 			
 			String line;
-			
+
+			Map<String, List<CityRecord>> map = new HashMap<String, List<CityRecord>>();
+
 			while ((line = br.readLine()) != null) {
 				// Parse each line
 				String[] rawValues = line.split(",");
@@ -23,12 +30,22 @@ public class CityCSVProcessor {
 				String city = convertToString(rawValues[2]);
 				int population = convertToInt(rawValues[3]);
 				
-				// System.out.println("id: " + id + ", year: " + year + ", city: " + city + ", population: " + population);
-				
 				//TODO: Extend the program to process entries!
-				processor.CityRecord cityRecord = new processor.CityRecord(id, year, city, population);
-				System.out.println(cityRecord);
+				CityRecord cityRecord = new CityRecord(id, year, population);
+				if (!map.containsKey(city))  {
+					map.put(city, new ArrayList<>());
+				}
+				map.get(city).add(cityRecord);
 			}
+
+			for (Entry<String, List<CityRecord>> entry : map.entrySet()) {
+				int population = 0;
+				for (CityRecord e : entry.getValue()) {
+					population += e.population;
+				}
+				System.out.println("Avg. pop. in " + entry.getKey() + " (" + entry.getValue().size() + " entries) " + population / entry.getValue().size());
+			}
+
 		} catch (Exception e) {
 			System.err.println("An error occurred:");
 			e.printStackTrace();
--- src/processor/CityRecord.java
+++ src/processor/CityRecord.java
@@ -3,17 +3,15 @@ package processor;
 class CityRecord {
     int id;
     int year;
-    String city;
     int population;
 
-    public CityRecord(int id, int year, String city, int population) {
+    public CityRecord(int id, int year, int population) {
         this.id = id;
         this.year = year;
-        this.city = city;
         this.population = population;
     }
 
     public String toString() {
-        return "id: " + this.id + ", year: " + this.year + ", city: " + this.city + ", population: " + this.population;
+        return "id: " + this.id + ", year: " + this.year + ", population: " + this.population;
     }
 }
